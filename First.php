e9b0956326265f7858eeb606f1d15e091c7fb2d4
onkarkurkute <onkarkurkute@gmail.com>
2014-09-02 09:18:46 -0400

    Changed First and Push

--- First.php
+++ First.php
@@ -2,13 +2,18 @@
 	<head>
 	</head>
 	<body>
+	<h1>Employee Details </h1>
 	<form action="second.php" method="post">
-	<p>Name: <input type="text" name="name" /><br />
-	Designation: <input type="text" name="designation" /></p>
+	<p>Name: <input type="text" name="name" />
+	</p>
+	<p>
+	  Designation: 
+	    <input type="text" name="designation" />
+	</p>
 	<p>Comments:<br />
-	<textarea name="comments" rows="5" cols="20"></textarea></p>
-
-	<p><input type="submit"></p>
+	  <textarea name="comments" rows="5" cols="20"></textarea>
+	</p>
+	<p><input type="submit" value="Submit"></p>
 	</form>	
 	</body>
 </html>
